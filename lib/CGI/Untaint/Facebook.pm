9b4da263e9f8331dc316fd38f35e976971de1a88
Nigel Horne <njh@bandsman.co.uk>
2023-07-12 16:35:20 -0700

    Layout of description is better

--- lib/CGI/Untaint/Facebook.pm
+++ lib/CGI/Untaint/Facebook.pm
@@ -147,9 +147,9 @@ Nigel Horne, C<< <njh at bandsman.co.uk> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-cgi-untaint-url-facebook at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=CGI-Untaint-Facebook>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+Please report any bugs or feature requests to C<bug-cgi-untaint-url-facebook at rt.cpan.org>,
+or through the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=CGI-Untaint-Facebook>.
+I will be notified, and then you'll automatically be notified of progress on your bug as I make changes.
 
 =head1 SEE ALSO
 
